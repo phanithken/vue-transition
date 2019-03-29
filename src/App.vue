491b393bb5158517efef9623e5ec41be38c97fe0
Ken Phanith <phanith.ken@quad.co.jp>
2019-03-29 15:49:16 +0900

    remove mode

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <transition name="fade" mode="out-in">
+    <transition name="fade">
       <router-view/>
     </transition>
   </div>
